e741c33a59a393eb66fd7990da988d4350ad0ce2
LFalcone <34078649+LFalcone@users.noreply.github.com>
2020-06-05 14:00:16 -0700

    changed database connector to work with our database

--- TodoRestService/src/main/java/com/uci/todorestservice/db/DatabaseConnector.java
+++ TodoRestService/src/main/java/com/uci/todorestservice/db/DatabaseConnector.java
@@ -13,13 +13,20 @@ public class DatabaseConnector {
     public static Connection getConnection() {
 
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Original
+            // Class.forName("org.mariadb.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
         try {
-            return DriverManager.getConnection("jdbc:mariadb://192.168.0.209/db4lecture",
-                    "root", "mytest");
+            return DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/cars?useSSL=false", 
+                    "root", "1234");
+            
+            // Original
+            // return DriverManager.getConnection("jdbc:mariadb://192.168.0.209/db4lecture",
+            //        "root", "mytest");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
